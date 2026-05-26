b5ea5890bb44510cb4b2a6793d37cd186956b1a2
Simon Ser <contact@emersion.fr>
2026-05-26 15:46:48 +0200

    refactor(view): narrow type for SVG element name constants
    By default, string literals yield values of type "string". Passing
    constants defined via string literals to a function doesn't allow
    the function to get precise information about the string contents,
    they just get a generic "string" type.
    
    SVG element names defined in StaticDomTags get passed to d3. d3
    would benefit from more precise information about input arguments:
    it can infer the element type based on the element name.
    
    Use "as const" to narrow down the effective type to the most precise
    one possible.
    
    More info on "as const":
    https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-4.html#const-assertions
    
    Signed-off-by: Simon Ser <contact@emersion.fr>

--- src/app/view/editor-main-view/data-views/static.dom.tags.ts
+++ src/app/view/editor-main-view/data-views/static.dom.tags.ts
@@ -46,9 +46,9 @@ export class StaticDomTags {
   /*
        GROUP object
      */
-  static SVG = "svg";
-  static GROUP_SVG = "g";
-  static GROUP_DOM_REF = "g";
+  static SVG = "svg" as const;
+  static GROUP_SVG = "g" as const;
+  static GROUP_DOM_REF = "g" as const;
 
   /*
       NODE (root group)
@@ -59,7 +59,7 @@ export class StaticDomTags {
        |- NODE_LABELAREA_TEXT        : text in-front of the node label are (drag node)
        |- NODE_CONNECTIONTIME_TEXT   : text in-front of the node connection time open detail view dialog on the right
      */
-  static NODE_SVG = "g";
+  static NODE_SVG = "g" as const;
   static NODE_ROOT_CONTAINER = "root_container_nodes";
   static NODE_ROOT_CONTAINER_DOM_REF = "g.root_container_nodes";
   static NODE_CLASS = "node";
@@ -74,59 +74,59 @@ export class StaticDomTags {
   static NODE_HAS_CONNECTIONS = "has_connections";
   static NODE_READONLY = "readonly";
 
-  static NODE_HOVER_ROOT_SVG = "rect";
+  static NODE_HOVER_ROOT_SVG = "rect" as const;
   static NODE_HOVER_ROOT_CLASS = "node_hover_root";
   static NODE_HOVER_ROOT_DOM_REF = "rect.node_hover_root";
 
-  static NODE_ROOT_SVG = "rect";
+  static NODE_ROOT_SVG = "rect" as const;
   static NODE_ROOT_CLASS = "node_root";
   static NODE_ROOT_DOM_REF = "rect.node_root";
 
-  static NODE_BACKGROUND_SVG = "rect";
+  static NODE_BACKGROUND_SVG = "rect" as const;
   static NODE_BACKGROUND_CLASS = "node_background";
   static NODE_BACKGROUND_DOM_REF = "rect.node_background";
 
-  static NODE_DOCKABLE_SVG = "rect";
+  static NODE_DOCKABLE_SVG = "rect" as const;
   static NODE_DOCKABLE_CLASS = "node_dockable";
   static NODE_DOCKABLE_DOM_REF = "rect.node_dockable";
 
-  static NODE_LABELAREA_SVG = "rect";
+  static NODE_LABELAREA_SVG = "rect" as const;
   static NODE_LABELAREA_CLASS = "node_labelarea";
   static NODE_LABELAREA_DOM_REF = "rect.node_labelarea";
 
-  static NODE_ANALYTICSAREA_SVG = "rect";
+  static NODE_ANALYTICSAREA_SVG = "rect" as const;
   static NODE_ANALYTICSAREA_CLASS = "node_analyticsarea";
   static NODE_ANALYTICSAREA_DOM_REF = "rect.node_analyticsarea";
 
-  static NODE_ANALYTICSAREA_TEXT_LEFT_SVG = "text";
+  static NODE_ANALYTICSAREA_TEXT_LEFT_SVG = "text" as const;
   static NODE_ANALYTICSAREA_TEXT_LEFT_CLASS = "node_title_analyticsarea_left";
   static NODE_ANALYTICSAREA_TEXT_LEFT_DOM_REF = "text.node_title_analyticsarea_left";
 
-  static NODE_ANALYTICSAREA_TEXT_RIGHT_SVG = "text";
+  static NODE_ANALYTICSAREA_TEXT_RIGHT_SVG = "text" as const;
   static NODE_ANALYTICSAREA_TEXT_RIGHT_CLASS = "node_title_analyticsarea_right";
   static NODE_ANALYTICSAREA_TEXT_RIGHT_DOM_REF = "text.node_title_analyticsarea_right";
 
-  static NODE_LABELAREA_TEXT_SVG = "text";
+  static NODE_LABELAREA_TEXT_SVG = "text" as const;
   static NODE_LABELAREA_TEXT_CLASS = "node_text";
   static NODE_LABELAREA_TEXT_DOM_REF = "text.node_text";
 
-  static NODE_CONNECTIONTIME_TEXT_SVG = "text";
+  static NODE_CONNECTIONTIME_TEXT_SVG = "text" as const;
   static NODE_CONNECTIONTIME_TEXT_CLASS = "node_connection_time";
   static NODE_CONNECTIONTIME_TEXT_DOM_REF = "text.node_connection_time";
 
-  static NODE_HOVER_DRAG_AREA_SVG = "path";
+  static NODE_HOVER_DRAG_AREA_SVG = "path" as const;
   static NODE_HOVER_DRAG_AREA_CLASS = "node_hover_drag_root";
   static NODE_HOVER_DRAG_AREA_DOM_REF = "path.node_hover_drag_root";
 
-  static NODE_HOVER_DRAG_AREA_BACKGROUND_SVG = "rect";
+  static NODE_HOVER_DRAG_AREA_BACKGROUND_SVG = "rect" as const;
   static NODE_HOVER_DRAG_AREA_BACKGROUND_CLASS = "node_hover_drag_root_background";
   static NODE_HOVER_DRAG_AREA_BACKGROUND_DOM_REF = "rect.node_hover_drag_root_background";
 
-  static NODE_EDIT_AREA_SVG = "path";
+  static NODE_EDIT_AREA_SVG = "path" as const;
   static NODE_EDIT_AREA_CLASS = "node_edit_button";
   static NODE_EDIT_AREA_DOM_REF = "path.node_edit_button";
 
-  static NODE_EDIT_AREA_BACKGROUND_SVG = "rect";
+  static NODE_EDIT_AREA_BACKGROUND_SVG = "rect" as const;
   static NODE_EDIT_AREA_BACKGROUND_CLASS = "node_edit_button_background";
   static NODE_EDIT_AREA_BACKGROUND_DOM_REF = "rect.node_edit_button_background";
 
@@ -145,7 +145,7 @@ export class StaticDomTags {
      |- EDGE_LINE_LAYER_2   : rendering layer 2
      |- EDGE_LINE_LAYER_3   : rendering layer 3
    */
-  static EDGE_SVG = "g";
+  static EDGE_SVG = "g" as const;
   static EDGE_ROOT_CONTAINER = "root_container_edges";
   static EDGE_ROOT_CONTAINER_DOM_REF = "g.root_container_edges";
   static EDGE_CLASS = "edge";
@@ -157,7 +157,7 @@ export class StaticDomTags {
   static EDGE_IS_END_NODE = "is_end_node";
   static EDGE_IS_NOT_END_NODE = "is_not_end_node";
 
-  static EDGE_LINE_SVG = "path";
+  static EDGE_LINE_SVG = "path" as const;
   static EDGE_LINE_CLASS = "edge_line";
   static EDGE_LINE_DOM_REF = "path.edge_line";
   static EDGE_LINE_LINE_ID = "trainrunId";
@@ -169,37 +169,37 @@ export class StaticDomTags {
   static EDGE_LINE_LAYER_3 = StaticDomTags.LINE_LAYER + "_3";
   static EDGE_LINE_GRAYEDOUT = StaticDomTags.TAG_GRAYEDOUT;
 
-  static EDGE_LINE_ARROW_SVG = "path";
+  static EDGE_LINE_ARROW_SVG = "path" as const;
   static EDGE_LINE_ARROW_CLASS = "edge_line_arrow";
   static EDGE_LINE_ARROW_DOM_REF = "path.edge_line_arrow";
   static TAG_LINE_ARROW_EDITOR = "editor";
 
-  static EDGE_LINE_PIN_SVG = "circle";
+  static EDGE_LINE_PIN_SVG = "circle" as const;
   static EDGE_LINE_PIN_CLASS = "edge_line_pin";
   static EDGE_LINE_PIN_DOM_REF = "circle.edge_line_pin";
   static EDGE_LINE_PIN_CONNECTION = "connection_pin";
   static EDGE_LINE_PIN_CONNECTION_DOM_REF = "circle.connection_line_pin";
 
-  static EDGE_LINE_STOP_ICON_SVG = "path";
+  static EDGE_LINE_STOP_ICON_SVG = "path" as const;
   static EDGE_LINE_STOP_ICON_CLASS = "edge_line_stop_arcs";
   static EDGE_LINE_STOP_ICON_CLASS_DOM_REF = "path.edge_line_stop_arcs";
 
-  static EDGE_LINE_TEXT_SVG = "text";
+  static EDGE_LINE_TEXT_SVG = "text" as const;
   static EDGE_LINE_TEXT_CLASS = "edge_text";
   static EDGE_LINE_TEXT_DOM_REF = "text.edge_text";
   static EDGE_LINE_TEXT_INDEX = "edge_text_index";
   static EDGE_DISABLE_EVENTS = "no_event";
   static EDGE_LINE_TEXT_ODD_FREQUENCY = "OddFrequency";
 
-  static EDGE_LINE_TEXT_BACKGROUND_SVG = "rect";
+  static EDGE_LINE_TEXT_BACKGROUND_SVG = "rect" as const;
   static EDGE_LINE_TEXT_BACKGROUND_CLASS = "edge_text_background";
   static EDGE_LINE_TEXT_BACKGROUND_DOM_REF = "rect.edge_text_background";
 
-  static EDGE_LINE_STOPS_GROUP_SVG = "g";
+  static EDGE_LINE_STOPS_GROUP_SVG = "g" as const;
   static EDGE_LINE_STOPS_GROUP_CLASS = "edge_line_stops_group";
   static EDGE_LINE_STOPS_GROUP_DOM_REF = "g.edge_line_stops_group";
 
-  static EDGE_LINE_STOPS_SVG = "circle";
+  static EDGE_LINE_STOPS_SVG = "circle" as const;
   static EDGE_LINE_STOPS_CLASS = "edge_line_stops";
   static EDGE_LINE_STOPS_DOM_REF = "circle.edge_line_stops";
   static EDGE_LINE_STOPS_INDEX = "StopIndex";
@@ -219,14 +219,14 @@ export class StaticDomTags {
   static TRANSITION_ROOT_CONTAINER_DOM_REF = "g.root_container_transition";
   static TRANSITION_GROUP_CLASS = "transition_group";
   static TRANSITION_GROUP_ATTR_TRANSITION_ID = "id";
-  static TRANSITION_LINE_SVG = "path";
+  static TRANSITION_LINE_SVG = "path" as const;
   static TRANSITION_LINE_CLASS = "transition_line";
   static TRANSITION_LINE_DOM_REF = "path.transition_line";
   static TRANSITION_LINE_CLASS_0 = StaticDomTags.LINE_LAYER + "_0";
   static TRANSITION_LINE_CLASS_1 = StaticDomTags.LINE_LAYER + "_1";
   static TRANSITION_LINE_CLASS_2 = StaticDomTags.LINE_LAYER + "_2";
   static TRANSITION_LINE_CLASS_3 = StaticDomTags.LINE_LAYER + "_3";
-  static TRANSITION_BUTTON_SVG = "polygon";
+  static TRANSITION_BUTTON_SVG = "polygon" as const;
   static TRANSITION_BUTTON_CLASS = "transition_pin";
   static TRANSITION_BUTTON_DOM_REF = "polygon.transition_pin";
   static TRANSITION_BUTTON_SIZE = 8;
@@ -243,7 +243,7 @@ export class StaticDomTags {
   static CONNECTION_ATTR_NODE_ID = "node_id";
   static CONNECTION_GROUP_CLASS = "connection_group";
   static CONNECTION_GROUP_ATTR_CONNECTION_ID = "id";
-  static CONNECTION_LINE_SVG = "path";
+  static CONNECTION_LINE_SVG = "path" as const;
   static CONNECTION_LINE_CLASS = "connection_line";
   static CONNECTION_LINE_DOM_REF = "path.connection_line";
   static CONNECTION_LINE_CLASS_0 = StaticDomTags.LINE_LAYER + "_0";
@@ -252,7 +252,7 @@ export class StaticDomTags {
   static CONNECTION_LINE_CLASS_3 = StaticDomTags.LINE_LAYER + "_3";
   static CONNECTION_TAG_ONGOING_DRAGGING = "connection_ongoing_dragging";
 
-  static CONNECTION_LINE_PIN_SVG = "circle";
+  static CONNECTION_LINE_PIN_SVG = "circle" as const;
   static CONNECTION_LINE_PIN_CLASS = "connection_line_pin";
   static CONNECTION_LINE_PIN_DOM_REF = "circle.connection_line_pin";
   static CONNECTION_ID = "id";
@@ -264,68 +264,68 @@ export class StaticDomTags {
   /*
     Multi Select Box
    */
-  static PREVIEW_MULTISELECT_ROOT_BOX_SVG = "g";
+  static PREVIEW_MULTISELECT_ROOT_BOX_SVG = "g" as const;
   static PREVIEW_MULTISELECT_ROOT_BOX_CLASS = "multi_select_box_root";
   static PREVIEW_MULTISELECT_ROOT_BOX_DOM_REF = "g.multi_select_box_root";
 
-  static PREVIEW_MULTISELECT_BOX_SVG = "rect";
+  static PREVIEW_MULTISELECT_BOX_SVG = "rect" as const;
   static PREVIEW_MULTISELECT_BOX_CLASS = "multi_select_box_root";
   static PREVIEW_MULTISELECT_BOX_DOM_REF = "rect.multi_select_box_root";
   /*
     PREVIEW_LINE
    */
-  static PREVIEW_LINE_ROOT_SVG = "g";
+  static PREVIEW_LINE_ROOT_SVG = "g" as const;
   static PREVIEW_LINE_ROOT_CLASS = "preview_line_root";
   static PREVIEW_LINE_ROOT_DOM_REF = "g.preview_line_root";
 
-  static PREVIEW_CONNECTION_LINE_ROOT_SVG = "g";
+  static PREVIEW_CONNECTION_LINE_ROOT_SVG = "g" as const;
   static PREVIEW_CONNECTION_LINE_ROOT_CLASS = "connection_preview_line_root";
   static PREVIEW_CONNECTION_LINE_ROOT_DOM_REF = "g.connection_preview_line_root";
 
-  static PREVIEW_LINE_SVG = "path";
+  static PREVIEW_LINE_SVG = "path" as const;
   static PREVIEW_LINE_CLASS = "preview_line";
   static PREVIEW_LINE_DOM_REF = "path.preview_line";
 
-  static PREVIEW_CONNECTION_LINE_SVG = "path";
+  static PREVIEW_CONNECTION_LINE_SVG = "path" as const;
   static PREVIEW_CONNECTION_LINE_CLASS = "preview_connection_line";
   static PREVIEW_CONNECTION_LINE_DOM_REF = "path.preview_connection_line";
 
   /* Note */
   static NOTE_ID = "id";
-  static NOTE_SVG = "g";
+  static NOTE_SVG = "g" as const;
   static NOTE_ROOT_CONTAINER = "root_container_notes";
   static NOTE_ROOT_CONTAINER_DOM_REF = "g.root_container_notes";
   static NOTE_CLASS = "note";
 
-  static NOTE_HOVER_ROOT_SVG = "rect";
+  static NOTE_HOVER_ROOT_SVG = "rect" as const;
   static NOTE_HOVER_ROOT_CLASS = "note_hover_root";
   static NOTE_HOVER_ROOT_DOM_REF = "rect.note_hover_root";
 
-  static NOTE_HOVER_DRAG_AREA_SVG = "path";
+  static NOTE_HOVER_DRAG_AREA_SVG = "path" as const;
   static NOTE_HOVER_DRAG_AREA_CLASS = "note_hover_drag_root";
   static NOTE_HOVER_DRAG_AREA_DOM_REF = "path.note_hover_drag_root";
 
-  static NOTE_HOVER_DRAG_AREA_BACKGROUND_SVG = "rect";
+  static NOTE_HOVER_DRAG_AREA_BACKGROUND_SVG = "rect" as const;
   static NOTE_HOVER_DRAG_AREA_BACKGROUND_CLASS = "note_hover_drag_root_background";
   static NOTE_HOVER_DRAG_AREA_BACKGROUND_DOM_REF = "rect.note_hover_drag_root_background";
 
-  static NOTE_ROOT_SVG = "rect";
+  static NOTE_ROOT_SVG = "rect" as const;
   static NOTE_ROOT_CLASS = "note_root";
   static NOTE_ROOT_DOM_REF = "rect.note_root";
 
-  static NOTE_TITELAREA_SVG = "rect";
+  static NOTE_TITELAREA_SVG = "rect" as const;
   static NOTE_TITELAREA_CLASS = "note_titelarea";
   static NOTE_TITELAREA_DOM_REF = "rect.note_titelarea";
 
-  static NOTE_TITELAREA_TEXT_SVG = "text";
+  static NOTE_TITELAREA_TEXT_SVG = "text" as const;
   static NOTE_TITELAREA_TEXT_CLASS = "titel_text";
   static NOTE_TITELAREA_TEXT_DOM_REF = "text.titel_text";
 
-  static NOTE_TEXTAREA_SVG = "rect";
+  static NOTE_TEXTAREA_SVG = "rect" as const;
   static NOTE_TEXTAREA_CLASS = "note_textarea";
   static NOTE_TEXTAREA_DOM_REF = "rect.note_textarea";
 
-  static NOTE_TEXT_SVG = "text";
+  static NOTE_TEXT_SVG = "text" as const;
   static NOTE_TEXT_CLASS = "note_text";
   static NOTE_TEXT_DOM_REF = "text.note_text";
 
